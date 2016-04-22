916c55425983346f382546854f63cdcceb126f5d
kukela <kukela@kukela-MacBook-Air.local>
2016-04-22 13:44:53 +0800

    源码

---
+++ CirStatisticalFigure/CirStatisticalFigure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CirStatisticalFigure
+//
+//  Created by kukela on 16/4/22.
+//  Copyright © 2016年 kukela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
